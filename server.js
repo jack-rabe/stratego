8888b79fdd69d472ef1146f0565df47fabfedfb3
MetalRain682237 <kellysmells7@yahoo.com>
2023-01-16 16:17:06 -0600

    updated server

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(path.resolve() + '/dist/index.html'));
 });
 app.use(express.static('dist'));
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+app.use('/assets', express.static(path.resolve() + '/dist'));
+
+let server = https
+    .createServer(
+        {
+            //must be after all the app...
+            key: fs.readFileSync('./ssl/server.key'),
+            cert: fs.readFileSync('./ssl/server.cert'),
+        },
+        app
+    )
+    .listen(443, `0.0.0.0`, function () {
+        console.log('Listening on port 443!');
+    });
