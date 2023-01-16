ead5294490637acc9e5edd99f51c08a7befd1124
Jack Rabe <jhrabe@wisc.edu>
2023-01-16 12:05:40 -0600

    add daisy UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [fruit, setFruit] = useState('peach')
 
   return (
     <div className="App">
-      <div className="flex">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+      <div className="flex border border-black mx-auto w-1/2 mt-5">
+        <a href="https://vitejs.dev" target="_blank" className='mx-auto my-3'>
+          <img src="/vite.svg"  alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://reactjs.org" target="_blank" className='mx-auto my-3' >
+          <img src={reactLogo} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className='flex flex-col'>
+        <button  onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button>
+        <button  className="btn" onClick={() => setFruit((fruit) => fruit == 'peach'? 'apple' : "peach")}>
+          Change Fruit from {fruit}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p>
         Click on the Vite and React logos to learn more
       </p>
     </div>
