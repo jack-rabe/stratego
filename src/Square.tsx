a611b7815fc43dfe5aa98a6d0fa69233a3c7d624
Jack Rabe <jhrabe@wisc.edu>
2023-01-16 16:12:05 -0600

    add Navbar

--- src/Square.tsx
+++ src/Square.tsx
@@ -1,19 +1,19 @@
 export default function Square(props: { position: number }) {
     return (
-        <div className={getBorderClasses(props.position)}>{props.position}</div>
+        <div
+            className={
+                'border-l border-t border-black bg-slate-700 text-center hover:bg-slate-500' +
+                getBorderClasses(props.position)
+            }
+        >
+            {props.position}
+        </div>
     );
 }
 
 // only add right right border to the far right elements and bottom borders to the bottom elements
 function getBorderClasses(position: number) {
-    let classes: string[] = [
-        'border-l',
-        'border-t',
-        'border-black',
-        'bg-slate-700',
-        'text-center',
-        'hover:bg-sky-700',
-    ];
+    let classes: string[] = [];
 
     let posStr: 'bottomRight' | 'bottom' | 'right' | 'middle' = 'middle';
     if (position < 90 && position % 10 != 9) posStr = 'middle';
